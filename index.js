bf38c2cc384501dd82c569b13187a45fff5b5544
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-12-11 23:27:40 +0300

    Add functionality to generate and display signed URL for viewing uploaded file

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import cors from "cors";
 import path from "path";
@@ -17,8 +21,8 @@ const s3 = new S3Client({
   endpoint: "http://localhost:9000", // MinIO's endpoint address (MinIO in docker runs on this port)
   region: "us-east-1", // Region (this doesn't really matter, in MinA this is usually 'us-east-1')
   credentials: {
-    accessKeyId: "YOUR-ACCESS-KEY", // MinIO access key
-    secretAccessKey: "YOUR-SECRET-KEY", // MinIO secret key
+    accessKeyId: "YOUR_ACCESS_KEY", // MinIO access key
+    secretAccessKey: "YOUR_SECRET_KEY", // MinIO secret key
   },
   forcePathStyle: true, // To be MinIO compatible
 });
@@ -41,9 +45,12 @@ app.get("/signed-url", async (req, res) => {
   };
 
   try {
+    const commandGet = new GetObjectCommand(params);
+    const signedUrlGet = await getSignedUrl(s3, commandGet, { expiresIn: 300 }); // Valid for 5 minutes
+
     const command = new PutObjectCommand(params);
     const signedUrl = await getSignedUrl(s3, command, { expiresIn: 300 }); // Valid for 5 minutes
-    res.json({ signedUrl });
+    res.json({ signedUrl, signedUrlGet });
   } catch (error) {
     console.error("Error generating signed URL:", error.message);
     res.status(500).send("Failed to generate signed URL");
