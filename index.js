324bb5f6a8549964482af3dc759302f28b4b8515
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-12-12 14:41:25 +0300

    Add support for environment variables

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
+const ACCESS_KEY = process.env.ACCESS_KEY;
+const SECRET_KEY = process.env.SECRET_KEY;
+const BUCKET_NAME = process.env.BUCKET_NAME;
 app.use(cors());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -21,8 +27,8 @@ const s3 = new S3Client({
   endpoint: "http://localhost:9000", // MinIO's endpoint address (MinIO in docker runs on this port)
   region: "us-east-1", // Region (this doesn't really matter, in MinA this is usually 'us-east-1')
   credentials: {
-    accessKeyId: "YOUR_ACCESS_KEY", // MinIO access key
-    secretAccessKey: "YOUR_SECRET_KEY", // MinIO secret key
+    accessKeyId: ACCESS_KEY, // MinIO access key
+    secretAccessKey: SECRET_KEY, // MinIO secret key
   },
   forcePathStyle: true, // To be MinIO compatible
 });
@@ -39,7 +45,7 @@ app.get("/signed-url", async (req, res) => {
   }
 
   const params = {
-    Bucket: "YOUR_BUCKET_NAME",
+    Bucket: BUCKET_NAME,
     Key: fileName,
     ContentType: contentType,
   };
